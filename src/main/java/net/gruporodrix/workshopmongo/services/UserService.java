7415e6ef06d53848c606f132524394cb9edd1b46
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2020-01-06 18:40:32 -0300

    Insert User with POST

--- src/main/java/net/gruporodrix/workshopmongo/services/UserService.java
+++ src/main/java/net/gruporodrix/workshopmongo/services/UserService.java
@@ -1,19 +1,36 @@
 package net.gruporodrix.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import net.gruporodrix.workshopmongo.domain.User;
+import net.gruporodrix.workshopmongo.dto.UserDTO;
 import net.gruporodrix.workshopmongo.repository.UserRepository;
+import net.gruporodrix.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 	@Autowired
 	private UserRepository repo;
+	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException(String.format("Requested id=%s does not exist", id)));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
 
